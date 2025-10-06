55044b82e5ce672581673105f8ec933d3ae2a6a4
Tess-Ri <28tess@gmail.com>
2025-10-06 15:59:12 +0200

    Create selfie_mode.m

---
+++ Code/selfie_mode.m
@@ -0,0 +1,79 @@
+clc, clear, close
+
+img = imread("amadus4.jpeg");
+img = imread('user_profile_stock.jpeg');
+%img = imread('Amadeus.jpeg');
+%img = imresize(img, 0.15);
+gray = rgb2gray(img);
+
+%th = 140;          %amadeus
+th = 100;           %stock image
+%th = 120;           %steve 2
+inv = 255 - gray;
+bw = inv > th;               % binary: figure = 1, background = 0
+
+figure(1)
+imshow(bw)
+title('Base binary mask');
+
+bw = bwareafilt(bw, 1);      % keep largest white object
+bw = imclose(bw, strel('disk', 20));   % close small gaps
+bw = bwareafilt(bw, 1);
+
+figure(2)
+imshow(bw)
+title('Base binary mask');
+
+[h, w] = size(bw);
+
+
+%fill the figure from up to down
+fill_updown = false(h, w);
+for c = 1:w
+    col = bw(:, c);
+    if any(col)
+        topIdx = find(col, 1, 'first');
+        fill_updown(topIdx:end, c) = true;
+    end
+end
+
+%fill the image from down to up
+fill_downup = false(h, w);
+for c = 1:w
+    col = bw(:, c);
+    if any(col)
+        botIdx = find(col, 1, 'last');
+        fill_downup(1:botIdx, c) = true;
+    end
+end
+
+%fill the image from left to right
+fill_leftright = false(h, w);
+for r = 1:h
+    row = bw(r, :);
+    if any(row)
+        leftIdx = find(row, 1, 'first');
+        fill_leftright(r, leftIdx:end) = true;
+    end
+end
+
+% Fill the image from right to left
+fill_rightleft = false(h, w);
+for r = 1:h
+    row = bw(r, :);
+    if any(row)
+        rightIdx = find(row, 1, 'last');
+        fill_rightleft(r, 1:rightIdx) = true;
+    end
+end
+
+
+combined_all = fill_updown & fill_downup & fill_leftright & fill_rightleft;
+
+
+figure(3);
+subplot(2,3,1); imshow(fill_updown); title('Up → Down');
+subplot(2,3,2); imshow(fill_downup); title('Down → Up');
+subplot(2,3,3); imshow(fill_leftright); title('Left → Right');
+subplot(2,3,4); imshow(fill_rightleft); title('Right → Left');
+subplot(2,3,5); imshow(combined_all); title('Intersection (All Directions)');
